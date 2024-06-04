a68e8adb60536a274f6c73d887e69b6475453b94
Ragavi <ragavirv21102003@gmail.com>
2024-06-04 22:07:53 +0530

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,4 @@
 
-
-
-const products=[{
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating:{
-    stars:4.5,
-    count:87
-  },
-  pricecents:1090
-},{
-  image:'images/products/intermediate-composite-basketball.jpg',
-  name:' Intermediate Size Basketball',
-  rating:{
-    stars:4,
-    count:127
-  },
-  pricecents:2095
-},{
-  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating:{
-    stars:4.5,
-    count:56
-  },
-  pricecents:799
-}];
 let productsHTML='';
 products.forEach((product)=>{
   productsHTML+=`<div class="product-container">
