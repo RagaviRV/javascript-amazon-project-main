e1c5c1c737661d78168f406d36f934819cc9fdbd
Ragavi <ragavirv21102003@gmail.com>
2024-06-05 18:51:41 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart=[];  
+export const cart=[{
+  productid:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2,
+},{
+  productid:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:1,
+} ];  
 
 export function addtocart(productid){
 
