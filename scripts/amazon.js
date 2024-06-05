539200b59eacb826331deb543cc23b4f4b69f0b5
Ragavi <ragavirv21102003@gmail.com>
2024-06-05 17:17:32 +0530

    import products from product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import {cart} from '../data/cart.js';
+import {products} from '../data/products.js';
 let productsHTML='';
 products.forEach((product)=>{
   productsHTML+=`<div class="product-container">
