f2dbd8fc3b1bffa1e6ac1390f85305dd80ab7f16
Ragavi <ragavirv21102003@gmail.com>
2024-06-13 16:38:30 +0530

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,87 @@
+class Cart{
+  cartItems;
+  localStorageKey;
+
+constructor(localStorageKey){
+  this.localStorageKey=localStorageKey;
+  this.loadFromStorage();
+ 
+  
+}
+
+  loadFromStorage(){
+    this.cartItems=JSON.parse(
+     localStorage.getItem(this.localStorageKey));
+     if(!this.cartItems){
+       this.cartItems= [{
+       productid:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+       quantity:2,
+       deliveryoptionsid:'1'
+     },{
+       productid:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+       quantity:1,
+       deliveryoptionsid:'2'
+     }];  
+     }
+  }
+
+  savetostorage(){
+    localStorage.setItem(this.localStorageKey,JSON.stringify(this.cartItems));
+   }
+
+   addtocart(productid){
+    let matchingItem;
+    this.cartItems.forEach((cartitem)=>{
+      const product_id=cartitem.productid ?? cartitem.productId;
+      if(productid===product_id){
+        matchingItem=cartitem;
+      }
+    });
+    if(matchingItem){
+      matchingItem.quantity+=1;
+    }
+    else{
+    this.cartItems.push({
+      productid:productid,
+      quantity:1,
+      deliveryoptionsid:'1'
+    });
+  }
+  
+  this.savetostorage();
+  }
+
+  removefromcart(productId){
+ 
+    const newcart=[];
+    this.cartItems.forEach((cartitem)=>{
+     const product_id=cartitem.productid ?? cartitem.productId;
+    if(product_id!=productId){
+     newcart.push(cartitem);
+    }
+    });
+   
+    this.cartItems=newcart;
+   this.savetostorage();
+   }
+   updatedeliveryoption(productid,deliveryoptionid){
+    let matchingItem;
+    this.cartItems.forEach((cartitem)=>{
+      const product_id=cartitem.productid ?? cartitem.productId;
+      if(productid===product_id){
+        matchingItem=cartitem;
+      }
+    });
+    matchingItem.deliveryoptionsid = deliveryoptionid;
+    this.savetostorage();
+  }
+}
+
+
+
+const cart=new Cart('cart-oop');
+const businessCart=new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+console.log('Instance:',businessCart instanceof Cart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderordersummary } from "./checkout/ordersummary.js";
 import {renderpaymentsummary} from './checkout/paymentsummary.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 renderordersummary();
 renderpaymentsummary();
